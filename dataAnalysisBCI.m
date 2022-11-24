3fb92e088cd1a2815f592119d279220e9111e7ea
Gaurav-1032201414 <96720032+Gaurav-1032201414@users.noreply.github.com>
2022-11-24 21:23:40 +0530

    Add files via upload

---
+++ dataAnalysisBCI.m
@@ -0,0 +1,39 @@
+
+% all_user_fp1 = DEAPdatasetAutoRecovered(:,1);
+% all_user_fp2 = DEAPdatasetAutoRecovered(:,17);
+% all_user_o1 = DEAPdatasetAutoRecovered(:,14);
+% all_user_o2 = DEAPdatasetAutoRecovered(:, end);
+
+% user01_fp1 = all_user_fp1(1:40, :);
+% user02_fp1 = all_user_fp1(41:80, :);
+% user03_fp1 = all_user_fp1(81:120, :);
+% user04_fp1 = all_user_fp1(121:160, :);
+% user05_fp1 = all_user_fp1(161:200, :);
+% user06_fp1 = all_user_fp1(201:240, :);
+
+% user01_fp2= all_user_fp2(1:40, :);
+% user02_fp2 = all_user_fp2(41:80, :);
+% user03_fp2 = all_user_fp2(81:120, :);
+% user04_fp2 = all_user_fp2(121:160, :);
+% user05_fp2 = all_user_fp2(161:200, :);
+% user06_fp2 = all_user_fp2(201:240, :);
+% 
+% user01_o1 = all_user_o1(1:40, :);
+% user02_o1 = all_user_o1(41:80, :);
+% user03_o1 = all_user_o1(81:120, :);
+% user04_o1 = all_user_o1(121:160, :);
+% user05_o1 = all_user_o1(161:200, :);
+% user06_o1 = all_user_o1(201:240, :);
+% 
+% user01_o2 = all_user_o2(1:40, :);
+% user02_o2 = all_user_o2(41:80, :);
+% user03_o2 = all_user_o2(81:120, :);
+% user04_o2 = all_user_o2(121:160, :);
+% user05_o2 = all_user_o2(161:200, :);
+% user06_o2 = all_user_o2(201:240, :);
+
+%% 
+
+
+
+
